4843719c12bb3e07204e9790240b114add00d70f
xd <artem082022@gmail.com>
2026-05-01 19:00:05 +0300

    Add someAbortable function to AsyncSome.js

--- AsyncSome.js
+++ AsyncSome.js
@@ -67,4 +67,56 @@ function somePromise(array, asyncPredicate) {
   });
 }
 
-module.exports = { someCallback, somePromise };
+function someAbortable(array, asyncPredicate, signal) {
+  return new Promise((resolve, reject) => {
+    if (signal && signal.aborted) {
+      return reject(new DOMException('Aborted', 'AbortError'));
+    }
+
+    if (array.length === 0) return resolve(false);
+
+    let index = 0;
+    let finished = false;
+
+    function abort() {
+      if (finished) return;
+      finished = true;
+      reject(new DOMException('Aborted', 'AbortError'));
+    }
+
+    if (signal) signal.addEventListener('abort', abort);
+
+    function next() {
+      if (finished) return;
+      if (index >= array.length) {
+        finished = true;
+        if (signal) signal.removeEventListener('abort', abort);
+        return resolve(false);
+      }
+
+      const current = index++;
+
+      asyncPredicate(array[current])
+        .then((result) => {
+          if (finished) return;
+          if (result) {
+            finished = true;
+            if (signal) signal.removeEventListener('abort', abort);
+            resolve(true);
+          } else {
+            next();
+          }
+        })
+        .catch((err) => {
+          if (finished) return;
+          finished = true;
+          if (signal) signal.removeEventListener('abort', abort);
+          reject(err);
+        });
+    }
+
+    next();
+  });
+}
+
+module.exports = { someCallback, somePromise, someAbortable };
